c71db3421193792d8ba746f88616e200a86a29ac
AndreicaPaul <paulpaulandreica@gmail.com>
2018-07-18 13:32:41 +0300

    Finish user and add Folding Cell List View

--- AndroidApp/app/src/main/java/ro/capac/android/capac2018/data/db/model/User.java
+++ AndroidApp/app/src/main/java/ro/capac/android/capac2018/data/db/model/User.java
@@ -1,42 +1,12 @@
-/*
- * Copyright (C) 2017 MINDORKS NEXTGEN PRIVATE LIMITED
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://mindorks.com/license/apache-v2
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License
- */
-
 package ro.capac.android.capac2018.data.db.model;
 
-import org.greenrobot.greendao.annotation.Entity;
-import org.greenrobot.greendao.annotation.Generated;
-import org.greenrobot.greendao.annotation.Id;
-import org.greenrobot.greendao.annotation.Property;
-
-/**
- * Created by janisharali on 08/12/16.
- */
-
-@Entity(nameInDb = "user")
 public class User {
-
-    @Id(autoincrement = true)
     private Long id;
 
-    @Property(nameInDb = "name")
     private String name;
 
-    @Property(nameInDb = "created_at")
     private String createdAt;
 
-    @Property(nameInDb = "updated_at")
     private String updatedAt;
 
     private String userName;
@@ -47,7 +17,6 @@ public class User {
 
     private String password;
 
-    @Generated(hash = 1381938396)
     public User(Long id, String name, String createdAt, String updatedAt, String userName,
             String emailAdress, String phoneNumber, String password) {
         this.id = id;
@@ -60,15 +29,7 @@ public class User {
         this.password = password;
     }
 
-    @Generated(hash = 586692638)
     public User() {
-    }
-
-    public User(String userName, String emailAdress, String phoneNumber, String password) {
-        this.userName = userName;
-        this.emailAdress = emailAdress;
-        this.phoneNumber = phoneNumber;
-        this.password = password;
     }
 
     public Long getId() {
